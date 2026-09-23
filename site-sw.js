8caf083c7fa043c0948db22afc74647800045536
bossnedvetskiy-source <boss.nedvetskiy@gmail.com>
2026-09-23 17:46:06 +0500

    Bump work app offline cache to v8

--- site-sw.js
+++ site-sw.js
@@ -1,4 +1,4 @@
-const VERSION='kuzdvor-offline-2026-09-23-v7';
+const VERSION='kuzdvor-offline-2026-09-23-v8';
 const SHELL_CACHE=VERSION+'-shell';
 const MEDIA_CACHE=VERSION+'-media';
 const API_CACHE=VERSION+'-api';
