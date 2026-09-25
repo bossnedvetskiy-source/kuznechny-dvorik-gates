b48d2ef09aa6db9d3daa0c52e57a42048c925fc2
bossnedvetskiy-source <boss.nedvetskiy@gmail.com>
2026-09-26 00:18:00 +0500

    Refresh offline cache for enhanced fence calculator

--- site-sw.js
+++ site-sw.js
@@ -1,4 +1,4 @@
-const VERSION='kuzdvor-offline-2026-09-25-v12';
+const VERSION='kuzdvor-offline-2026-09-25-v13';
 const SHELL_CACHE=VERSION+'-shell';
 const MEDIA_CACHE=VERSION+'-media';
 const API_CACHE=VERSION+'-api';
