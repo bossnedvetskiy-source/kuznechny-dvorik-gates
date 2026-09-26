7a1800f5f1c340f86398fb41f60a1c51b2f74df6
bossnedvetskiy-source <boss.nedvetskiy@gmail.com>
2026-09-26 14:17:14 +0500

    Refresh cache after removing fence color selector

--- site-sw.js
+++ site-sw.js
@@ -1,4 +1,4 @@
-const VERSION='kuzdvor-offline-2026-09-26-v25';
+const VERSION='kuzdvor-offline-2026-09-26-v26';
 const SHELL_CACHE=VERSION+'-shell';
 const MEDIA_CACHE=VERSION+'-media';
 const API_CACHE=VERSION+'-api';
