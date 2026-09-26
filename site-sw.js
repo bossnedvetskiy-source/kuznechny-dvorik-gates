f5d2c3491ce4472b261cfcde3e40b2da771388ee
bossnedvetskiy-source <boss.nedvetskiy@gmail.com>
2026-09-26 12:15:24 +0500

    Refresh offline cache for gate-wicket fence geometry

--- site-sw.js
+++ site-sw.js
@@ -1,4 +1,4 @@
-const VERSION='kuzdvor-offline-2026-09-25-v13';
+const VERSION='kuzdvor-offline-2026-09-26-v14';
 const SHELL_CACHE=VERSION+'-shell';
 const MEDIA_CACHE=VERSION+'-media';
 const API_CACHE=VERSION+'-api';
