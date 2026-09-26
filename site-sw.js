381583b02db7d55426ad6a64cab300333993e098
bossnedvetskiy-source <boss.nedvetskiy@gmail.com>
2026-09-26 13:31:59 +0500

    Refresh offline cache for rounded euro-picket visuals

--- site-sw.js
+++ site-sw.js
@@ -1,4 +1,4 @@
-const VERSION='kuzdvor-offline-2026-09-26-v19';
+const VERSION='kuzdvor-offline-2026-09-26-v20';
 const SHELL_CACHE=VERSION+'-shell';
 const MEDIA_CACHE=VERSION+'-media';
 const API_CACHE=VERSION+'-api';
